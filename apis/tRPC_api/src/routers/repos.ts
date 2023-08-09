aab02e51b72c63f31db63cc2939871602aa241f3
Maciej Bednarski <maciej.bednarski@nokia.com>
2023-08-10 00:28:44 +0200

    final version with performance tests

--- apis/tRPC_api/src/routers/repos.ts
+++ apis/tRPC_api/src/routers/repos.ts
@@ -1,7 +1,15 @@
 import { publicProcedure, router } from '../trpc';
 import { z } from 'zod';
 import { RepoI } from '../types';
-import { findAllRepos, findRepoById } from '../mongoFunctions';
+import {
+    findAllRepos,
+    findRepoById,
+    findAllCommitsById,
+    findAllContributorsById,
+    findAllIssuesById,
+    findAllReleasesById,
+    findAllLanguagesById,
+} from '../mongoFunctions';
 
 export const reposRouter = router({
     all: publicProcedure.query(async (req) => {
@@ -11,5 +19,14 @@ export const reposRouter = router({
     byId: publicProcedure.input(z.number()).query(async (req) => {
         const repo: RepoI | null = await findRepoById(req.input);
         return repo;
+    }),
+    allinfo: publicProcedure.input(z.number()).query(async (req) => {
+        const langs = await findAllLanguagesById(req.input);
+        const issue = await findAllIssuesById(req.input);
+        const contrib = await findAllContributorsById(req.input);
+        const releases = await findAllReleasesById(req.input);
+        const commits = await findAllCommitsById(req.input);
+        const response = { ...langs, ...issue, ...contrib, ...releases, ...commits };
+        return response;
     }),
 });
